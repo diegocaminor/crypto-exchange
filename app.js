d2a4205f971c0758fc4686af7a69ca6f66906019
Diego Camino <diego.camino.r@gmail.com>
2020-04-18 23:49:41 -0500

    Computed Properties y Watchers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = new Vue({
   data () {
       return {
         name: 'Bitcoin',
+        symbol: 'BTC',
         img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
         changePercent: 0,
         color: 'f4f4f4',
@@ -19,6 +20,22 @@ const app = new Vue({
         showPrices: false
       }
   },
+  // Propiedades computadas (Computed) = propiedades que se calculan en tiempo real en base a los valores de otras propiedades.
+  // Watcher = Funciones que ejecutan un código es decir que por medio de un cambio de la observación de una variable se puede disparar determinado código o ejecutar una función.
+  // Podemos pensarlo como un Disparador de código.
+  computed: {
+    title() {
+      return ` ${this.name} + ${this.symbol}`
+    }
+  },
+
+  // El nombre de la función se tiene que corresponder con el nombre de una propiedad en data
+  watch: {
+    showPrices(newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
+  },
+
   methods: {
     toogleShowPrices() {
       this.showPrices = !this.showPrices
