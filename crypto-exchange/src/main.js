b0b5ced833868e6106702b2e1e40f0a169f8b16c
Diego Camino <diego.camino.r@gmail.com>
2020-04-21 01:13:32 -0500

    Consumo de API Coincap utilizando fetch

--- crypto-exchange/src/main.js
+++ crypto-exchange/src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "@/assets/css/tailwind.css"; // el  @ es un pequeño truco que nos da webpack y vue para hacer referencia de manera relativa al directorio src
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css' // el  @ es un pequeño truco que nos da webpack y vue para hacer referencia de manera relativa al directorio src
 
-import routes from "@/routes";
+import routes from '@/routes'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router: routes,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
--- crypto-exchange/src/routes.js
+++ crypto-exchange/src/routes.js
@@ -1,32 +1,32 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
-import Home from "@/views/Home";
-import Error from "@/views/Error";
-import About from "@/views/About";
+import Home from '@/views/Home'
+import Error from '@/views/Error'
+import About from '@/views/About'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
 
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home,
+      path: '/',
+      name: 'home',
+      component: Home
     },
 
     {
-      path: "/about",
-      name: "about",
-      component: About,
+      path: '/about',
+      name: 'about',
+      component: About
     },
 
     {
-      path: "*",
-      name: "error",
-      component: Error,
-    },
-  ],
-});
+      path: '*',
+      name: 'error',
+      component: Error
+    }
+  ]
+})
