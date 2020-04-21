300cdd92bd8129b68eb422783b41de191c9bd00a
Diego Camino <diego.camino.r@gmail.com>
2020-04-20 23:57:34 -0500

    Adición de rutas home, about y error con vue-router

--- crypto-exchange/src/main.js
+++ crypto-exchange/src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css"; // el  @ es un pequeño truco que nos da webpack y vue para hacer referencia de manera relativa al directorio src
 
+import routes from "@/routes";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  router: routes,
+  render: (h) => h(App),
 }).$mount("#app");
