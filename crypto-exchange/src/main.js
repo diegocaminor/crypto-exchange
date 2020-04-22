a30631c60aff992b834134c9663dbb4a6fb89d10
Diego Camino <diego.camino.r@gmail.com>
2020-04-22 01:20:33 -0500

    Integración componentes de terceros: vue-spinners y vue-chartkick

--- crypto-exchange/src/main.js
+++ crypto-exchange/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css' // el  @ es un pequeño truco que nos da webpack y vue para hacer referencia de manera relativa al directorio src
+import Chart from 'chart.js'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
 
 import routes from '@/routes'
 import { dollarFilter, percentFilter } from '@/filters'
+
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
 
 Vue.filter('dollar', dollarFilter)
 Vue.filter('percent', percentFilter)
